d59425cbf2609b61bc60347d0bc82830a03bcf38
Kevin <kpatrick@my.wctc.edu>
2018-10-24 20:55:59 -0500

    Initial commit

---
+++ test/independentcopy/ListReaderTest.java
@@ -0,0 +1,26 @@
+package independentcopy;
+import independentcopy.Driver;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+public class ListReaderTest {
+
+
+
+    @Test
+    public void TestReadln() {
+        assertTrue(1==1);
+        assertFalse(1==2);
+    }
+}
